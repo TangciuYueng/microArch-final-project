6ff150d74cd0e22e309dd21b737086b47c267c54
SU+M <1191044710@qq.com>
2023-11-21 20:47:20 +0800

    新建了musicListen这个Module

---
+++ heartWave/musicListen/src/main/java/cn/edu/tongji/musicListen/MusicListenApplication.java
@@ -0,0 +1,11 @@
+package cn.edu.tongji.musicListen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicListenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MusicListenApplication.class, args);
+    }
+}
