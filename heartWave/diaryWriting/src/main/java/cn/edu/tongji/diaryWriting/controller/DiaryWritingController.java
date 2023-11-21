a8129851d907de82bedb4174701a2578228042a6
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2023-11-21 22:24:34 +0800

    新建文件夹

--- heartWave/diaryWriting/src/main/java/cn/edu/tongji/diaryWriting/controller/DiaryWritingController.java
+++ heartWave/diaryWriting/src/main/java/cn/edu/tongji/diaryWriting/controller/DiaryWritingController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Test {
+public class DiaryWritingController {
     @RequestMapping("/test")
     public String test() {
         return "Heart wave!";
