10dc53f05e2bd52ded2dc457e62f8572c5b4df3d
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2023-11-21 21:44:43 +0800

    新增diaryWriting

---
+++ heartWave/diaryWriting/src/main/java/cn/edu/tongji/diaryWriting/DiaryWritingApplication.java
@@ -0,0 +1,11 @@
+package cn.edu.tongji.diaryWriting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryWritingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryWritingApplication.class, args);
+    }
+}
