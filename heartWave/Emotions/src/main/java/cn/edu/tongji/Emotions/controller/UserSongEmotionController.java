2e82bf2c50a75542585c1407c6692fe395cd4bc3
精神测绘人 <80401491+IkMangMok@users.noreply.github.com>
2023-11-28 20:34:30 +0800

    api修改

--- heartWave/Emotions/src/main/java/cn/edu/tongji/Emotions/controller/UserSongEmotionController.java
+++ heartWave/Emotions/src/main/java/cn/edu/tongji/Emotions/controller/UserSongEmotionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/userSongEmotions")
+@RequestMapping("/api/emotion/serSongEmotions")
 public class UserSongEmotionController {
 
     @Resource
