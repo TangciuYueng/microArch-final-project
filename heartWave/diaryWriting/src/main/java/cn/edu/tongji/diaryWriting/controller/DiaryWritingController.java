13d49e60359160cd7984ff9c6562e7174abfb3d0
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2023-11-30 20:46:31 +0800

    格式更新

--- heartWave/diaryWriting/src/main/java/cn/edu/tongji/diaryWriting/controller/DiaryWritingController.java
+++ heartWave/diaryWriting/src/main/java/cn/edu/tongji/diaryWriting/controller/DiaryWritingController.java
@@ -45,61 +45,81 @@ public class DiaryWritingController {
 
     // 为指定用户创建新的日记（成功）
     @PostMapping("/add")
-    public ResponseEntity<Integer> createDiary(@RequestBody AddDiaryWritingRequest diaryRequest) {
-        Date currentDate = new Date();
-        // 创建一个格式化日期的对象
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        // 将日期对象格式化成字符串
-        String dateString = df.format(currentDate);
-        diaryRequest.setCreateDate(dateString);
-        System.out.println(diaryRequest.getCreateDate());
-        diaryRequest.setCurrentStatus(1);//1表示发布 2表示尚未发布在草稿箱中等待修改
-        int newDiaryId = diaryWritingService.createDiary(diaryRequest);
-        return ResponseEntity.ok(newDiaryId);
+    public ResponseEntity<?> createDiary(@RequestBody AddDiaryWritingRequest diaryRequest) {
+        try {
+            Date currentDate = new Date();
+            // 创建一个格式化日期的对象
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            // 将日期对象格式化成字符串
+            String dateString = df.format(currentDate);
+            diaryRequest.setCreateDate(dateString);
+            System.out.println(diaryRequest.getCreateDate());
+            diaryRequest.setCurrentStatus(1);//1表示发布 2表示尚未发布在草稿箱中等待修改
+            int newDiaryId = diaryWritingService.createDiary(diaryRequest);
+            return new ResponseEntity<>(newDiaryId, HttpStatusCode.valueOf(HttpStatus.SC_CREATED));
+        }catch(Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("add new diary failed", HttpStatusCode.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR));
+        }
     }
 
     //删除指定日记（完成）
     @DeleteMapping("/delete/{id}")
-    public int deleteDiaryByID(@PathVariable int id) {
+    public ResponseEntity<?> deleteDiaryByID(@PathVariable int id) {
+        try{
+            int deleteId=diaryWritingService.deleteDiary(id);
+            return new ResponseEntity<>(deleteId, HttpStatusCode.valueOf(HttpStatus.SC_OK));
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("delete diary failed", HttpStatusCode.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR));
+        }
         // 根据id删除数据的逻辑处理
-        return diaryWritingService.deleteDiary(id);
+
     }
 
     //用户修改随笔
     // 更新日记的接口
     @PutMapping("/put/{id}")
-    public ResponseEntity<String> updateDiary(@PathVariable int id, @RequestBody DiaryWritingInfo diary) {
+    public ResponseEntity<?> updateDiary(@PathVariable int id, @RequestBody DiaryWritingInfo diary) {
         //如果日记是已发布的状态，那么就把状态改为修改过了
-        Diary previousDiary=diaryWritingService.getDiaryById(id);
+        try {
+            Diary previousDiary = diaryWritingService.getDiaryById(id);
 //        System.out.println(previousDiary.getCurrentStatus());
 //        System.out.println(diary.getCurrentStatus());
-        if(diary.getCurrentStatus()==0){
-            if(previousDiary.getCurrentStatus()==1 || previousDiary.getCurrentStatus()==3){
+            if (diary.getCurrentStatus() == 0) {
+                if (previousDiary.getCurrentStatus() == 1 || previousDiary.getCurrentStatus() == 3) {
 //            System.out.println(previousDiary.getCurrentStatus());
-                diary.setCurrentStatus(3);
-            }
-            else if(previousDiary.getCurrentStatus()==2){//如果日记是在草稿箱中未发布，那么还是保持状态2
+                    diary.setCurrentStatus(3);
+                } else if (previousDiary.getCurrentStatus() == 2) {//如果日记是在草稿箱中未发布，那么还是保持状态2
 //            System.out.println(previousDiary.getCurrentStatus());
-                diary.setCurrentStatus(2);
+                    diary.setCurrentStatus(2);
+                }
             }
+
+            Date currentDate = new Date();
+            // 创建一个格式化日期的对象
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            // 将日期对象格式化成字符串
+            String dateString = df.format(currentDate);
+            diary.setUpdateTime(dateString);//设置更新时间
+            diaryWritingService.updateDiary(id, diary);
+            return new ResponseEntity<>("put diary successfully", HttpStatusCode.valueOf(HttpStatus.SC_OK));
+        }catch(Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("put diary failed", HttpStatusCode.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR));
         }
-
-        Date currentDate = new Date();
-        // 创建一个格式化日期的对象
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        // 将日期对象格式化成字符串
-        String dateString = df.format(currentDate);
-        diary.setUpdateTime(dateString);//设置更新时间
-        diaryWritingService.updateDiary(id,diary);
-        return ResponseEntity.ok("Diary updated successfully.");
-
     }
 
     //获得某个用户的日记总数
     @GetMapping("/count/{userId}")
-    public ResponseEntity<Integer> getDiaryCount(@PathVariable("userId") int userId) {
-        int count = diaryWritingService.diaryCount(userId);
-        return ResponseEntity.ok().body(count);
+    public ResponseEntity<?> getDiaryCount(@PathVariable("userId") int userId) {
+        try {
+            int count = diaryWritingService.diaryCount(userId);
+            return new ResponseEntity<>(count, HttpStatusCode.valueOf(HttpStatus.SC_OK));
+        } catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("get diaries count failed", HttpStatusCode.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR));
+        }
     }
 
 
@@ -118,17 +138,22 @@ public class DiaryWritingController {
         }
         catch (Exception e){
             e.printStackTrace();
-            return new ResponseEntity<>("get diaries by user Id failed", HttpStatusCode.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR));
+            return new ResponseEntity<>("get diaries by Id failed", HttpStatusCode.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR));
         }
     }
 
     //获得指定id日记的情绪分析
     @GetMapping("/emotionAnalysis/{id}")
-    public ResponseEntity<SentimentData> getDiaryEmotion(@PathVariable("id") int id) {
+    public ResponseEntity<?> getDiaryEmotion(@PathVariable("id") int id) {
+        try{
         Diary diary = diaryWritingService.getDiaryById(id);
         String content = diary.getContent();
         SentimentData sentimentData = baiduSentimentAnalysis.sentimentAnalysis(content);
-        return ResponseEntity.ok().body(sentimentData);
+        return new ResponseEntity<>(sentimentData,HttpStatusCode.valueOf(HttpStatus.SC_OK));
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>("analyze diaries emotion failed", HttpStatusCode.valueOf(HttpStatus.SC_INTERNAL_SERVER_ERROR));
+        }
     }
 
 
