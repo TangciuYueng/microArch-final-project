f1a6a248ef4b03b4f0c24b6d94206b9ace1b9dce
TangciuYueng <2310360939@qq.com>
2023-12-08 15:55:11 +0800

    chat room in springboot demo

--- chatRoomTestJava/NettyTest/src/main/java/cn/edu/tongji/WebSocketChatServerInitializer.java
+++ chatRoomTestJava/NettyTest/src/main/java/cn/edu/tongji/WebSocketChatServerInitializer.java
@@ -9,16 +9,20 @@ import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class WebSocketChatServerInitializer extends
-		ChannelInitializer<SocketChannel> {	//（1）
+		ChannelInitializer<SocketChannel> {
 
 	@Override
-    public void initChannel(SocketChannel ch) throws Exception {//（2）
-		 ChannelPipeline pipeline = ch.pipeline();
+    public void initChannel(SocketChannel ch) throws Exception {
+		// 获取与客户端通信的 pipeline
+		ChannelPipeline pipeline = ch.pipeline();
 
         pipeline.addLast(new HttpServerCodec());
 		pipeline.addLast(new HttpObjectAggregator(64*1024));
+		// 支持异步写大数据流，不会占用过多的内存，防止发生内存溢出
 		pipeline.addLast(new ChunkedWriteHandler());
+		// 处理 HTTP 请求，将 WebSocket 升级请求转发到指定路径
 		pipeline.addLast(new HttpRequestHandler("/ws"));
+		// 处理 WebSocket 协议的握手和控制帧
 		pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
 		pipeline.addLast(new TextWebSocketFrameHandler());
 
