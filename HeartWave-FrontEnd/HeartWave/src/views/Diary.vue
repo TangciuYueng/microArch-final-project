6e1fdfb2ab0fda0f78c5a49791ad26746ec61870
deidei1210 <1548374581@qq.com>
2023-12-20 18:39:05 +0800

    日记前端初步，还没搞定怎么显示日期

--- HeartWave-FrontEnd/HeartWave/src/views/Diary.vue
+++ HeartWave-FrontEnd/HeartWave/src/views/Diary.vue
@@ -1,7 +1,19 @@
 <template>
     <!-- 顶部的导航栏 -->
     <DefaultBar />
-    日记
+    <v-container fluid>
+        <v-row>
+            <v-col v-for="day in days" :key="day" class="text-center">
+                {{ day }}
+            </v-col>
+        </v-row>
+        <v-divider></v-divider>
+        <v-row v-for="week in weeks" :key="week">
+            <v-col v-for="day in days" :key="day" class="text-center">
+                {{ week * 7 + day - startDay }}
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
   
 <script>
@@ -17,7 +29,15 @@ export default {
         userAccount: null,
         password: null,
         loading: false,
+        days: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+        startDay: 3, // Wednesday
+        numWeeks: 5,
     }),
+    computed: {
+        weeks() {
+            return Array.from({ length: this.numWeeks }, (_, i) => i);
+        },
+    },
     methods: {
 
     },
