d048d0d1071e7004dc2efbb276ee7a4751aa1dc4
BoringMarsh <279623342@qq.com>
2024-01-01 20:45:34 +0800

    修复feign依赖

--- heartWave/friend/src/main/java/cn/edu/tongji/friend/FriendApplication.java
+++ heartWave/friend/src/main/java/cn/edu/tongji/friend/FriendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(basePackages = "cn.edu.tongji.friend")
 public class FriendApplication {
     public static void main(String[] args) {
         SpringApplication.run(FriendApplication.class, args);
