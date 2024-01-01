6233db77b123984876877f08ff395afe3a699d5e
BoringMarsh <279623342@qq.com>
2024-01-01 22:40:02 +0800

    删除多余文件

--- heartWave/friend/src/main/java/cn/edu/tongji/friend/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.tongji.friend.repository;
-
-import cn.edu.tongji.friend.model.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, String> {
-    //不用编写，有一套已实现的基本方法
-}
