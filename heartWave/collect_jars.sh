c2b72ab4d01df01d40690900c564ac97c41ce00e
TangciuYueng <2310360939@qq.com>
2024-01-01 21:14:27 +0800

    add collect_jars.sh

---
+++ heartWave/collect_jars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 创建名为jars的新文件夹
+mkdir -p jars
+
+# 搜索当前目录下的所有文件夹
+for dir in */; do
+  if [ -d "$dir" ]; then
+    # 如果文件夹中有target文件夹
+    if [ -d "${dir}target" ]; then
+      # 拷贝target文件夹中的*jar文件到jars文件夹中
+      cp -n "${dir}target"/*.jar jars
+    fi
+  fi
+done
