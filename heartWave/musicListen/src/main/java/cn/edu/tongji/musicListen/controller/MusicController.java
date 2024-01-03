9fab80d755b76641eb82f96f96398258b8b443a4
SU+M <1191044710@qq.com>
2024-01-03 20:14:59 +0800

    Update MusicController.java

--- heartWave/musicListen/src/main/java/cn/edu/tongji/musicListen/controller/MusicController.java
+++ heartWave/musicListen/src/main/java/cn/edu/tongji/musicListen/controller/MusicController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/music_listen/music")
+@RequestMapping("/api/music-listen/music")
 public class MusicController {
     @Resource
     private MusicService musicService;
