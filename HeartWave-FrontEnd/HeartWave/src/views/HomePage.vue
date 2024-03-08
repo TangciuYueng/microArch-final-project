d90556ef8268fdef850a38cc5fdc6334b876d479
deidei1210 <78363795+deidei1210@users.noreply.github.com>
2024-03-08 19:40:20 +0800

    一些没上传的前端的东西

--- HeartWave-FrontEnd/HeartWave/src/views/HomePage.vue
+++ HeartWave-FrontEnd/HeartWave/src/views/HomePage.vue
@@ -5,16 +5,21 @@
             <v-row style="min-height:88vh;">
                 <!-- 第一列，宽度为第二列的3倍，背景颜色为白色 -->
                 <v-col cols="8" class="white-background scrollable-column">
-                    <!-- 这里放置第一列的内容 -->
+                    <v-row>
+                        <v-responsive max-width="350" max-height="50">
+                            <v-spacer></v-spacer>
+                            <v-text-field clearable label="请输入搜索关键词"
+                                prepend-icon="mdi-magnify"></v-text-field></v-responsive>
+                    </v-row>
                     <!-- 第一行，显示今日天气、天气小贴士、以及需要关心的朋友 -->
                     <v-row>
                         <v-col>
                             <!-- 天气卡片 -->
-                            <v-row></v-row>
+                            <v-row><weather-card></weather-card></v-row>
                             <!-- 需要关心的人 -->
                             <v-row></v-row>
                         </v-col>
-                        
+
                         <!-- 天气小贴士 -->
                         <v-col>
 
@@ -23,13 +28,13 @@
 
                     <!-- 推荐的歌单 -->
                     <v-row>
-                    
+
                     </v-row>
 
                     <!-- 推荐的音乐室 -->
                     <v-row>
                     </v-row>
-                
+
                 </v-col>
                 <!-- 第二列，背景颜色为绿色 -->
                 <v-col cols="4" class="green-background scrollable-column">
@@ -41,7 +46,11 @@
 </template>
   
 <script>
+import WeatherCard from "@/components/WeatherCard.vue"
 export default {
+    components: {
+        WeatherCard,
+    },
     //导出组件
     data: () => ({
         form: false,
@@ -74,4 +83,5 @@ export default {
     /* 在垂直方向上溢出时显示垂直滚动条 */
     max-height: 88vh;
     /* 设置最大高度为88vh，超出部分将显示滚动条 */
-}</style>
+}
+</style>
