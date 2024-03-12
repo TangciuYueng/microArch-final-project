af315f9101824fdb180865277485fd37b6bbd72c
TangciuYueng <2310360939@qq.com>
2024-03-12 13:11:39 +0800

    Update MusicSearch.vue

--- HeartWave-FrontEnd/HeartWave/src/views/MusicSearch.vue
+++ HeartWave-FrontEnd/HeartWave/src/views/MusicSearch.vue
@@ -5,10 +5,10 @@
                 <v-col cols="8">
                     <v-row>
                         <v-col col="6">
-                            <span>
+                            <span style="color: rgb(60, 153, 130); font-size:xx-large; font-weight: bold;">
                                 {{ keyWord }}
                             </span>
-                            <span>
+                            <span style="font-size: medium; font-style: oblique;">
                                 找到{{ singleMusicNum }}首单曲，{{ musicListNum }}个歌单，{{ musicRoomNum }}个音乐室
                             </span>
                         </v-col>
@@ -19,7 +19,14 @@
                         </v-col>
                     </v-row>
 
-                    <v-data-table :header="headers" :items="musics">
+                    <div class="tabs">
+                        <div v-for="(tab, index) in tabs" :key="index"
+                            :class="{ 'tab': true, 'active': activeTab === index }" @click="handleActiveTab(index)">
+                            {{ tab }}
+                        </div>
+                    </div>
+
+                    <v-data-table :items="selectedList">
 
                     </v-data-table>
                 </v-col>
@@ -110,18 +117,32 @@ export default {
         musicListNum: 80,
         musicRoomNum: 33,
 
-        headers: [
-            { text: '音乐名称', value: 'musicName' },
-            { text: '歌手', value: 'singer' },
-            { text: '专辑', value: 'album' },
-            { text: '时长', value: 'duration' },
-        ],
-        musics: [
-            { musicName: '快乐崇拜', singer: '潘玮柏/张韶涵', album: 'aa', duration: 24 },
-            { musicName: '最冷一天', singer: '张国荣', album: 'bb', duration: 37 },
-            { musicName: '負けないで', singer: 'ZARD', album: 'cc', duration: 24 },
-            { musicName: '愛の雫', singer: '忘了', album: 'dd', duration: 67 },
-            { musicName: '北酒場', singer: '中森明菜', album: 'ff', duration: 49 },
+        selectedList: [],
+
+        totalList: [
+            [
+                { '音乐名称': '快乐崇拜', '歌手': '潘玮柏/张韶涵', '专辑': 'aa', '时长': 24 },
+                { '音乐名称': '最冷一天', '歌手': '张国荣', '专辑': 'bb', '时长': 37 },
+                { '音乐名称': '負けないで', '歌手': 'ZARD', '专辑': 'cc', '时长': 24 },
+                { '音乐名称': '愛の雫', '歌手': '忘了', '专辑': 'dd', '时长': 67 },
+                { '音乐名称': '北酒場', '歌手': '中森明菜', '专辑': 'ff', '时长': 49 },
+            ],
+            [
+                { id: 1, name: "Relaxing Vibes", owner: "John Doe", tracks: 15 },
+                { id: 2, name: "Workout Hits", owner: "Jane Smith", tracks: 10 },
+            ],
+            [
+                { id: 1, name: "Chill Zone", members: 20, privacy: "public" },
+                { id: 2, name: "Jam Session", members: 15, privacy: "private" },
+            ],
+            [
+                { id: 1, title: "Golden Melodies", artist: "Alicia Keys", year: 2022 },
+                { id: 2, title: "Summer Breeze", artist: "Maroon 5", year: 2021 },
+            ],
+            [
+                { id: 1, username: "musiclover123", followers: 500, following: 300 },
+                { id: 2, username: "rockstar22", followers: 1000, following: 800 },
+            ]
         ],
 
         profileIcons: [
@@ -148,10 +169,18 @@ export default {
         emotionValue: 78,
         visitorNum: 234,
 
+        tabs: ['单曲', '歌单', '音乐室', '专辑', '用户'],
+        activeTab: 0,
     }),
     methods: {
-
+        handleActiveTab(index) {
+            this.activeTab = index;
+            this.selectedList = this.totalList[index];
+        }
     },
+    mounted() {
+        this.selectedList = this.totalList[0];
+    }
 }
 </script>
 
@@ -166,4 +195,30 @@ export default {
     color: white;
 }
 
+.tabs {
+    border-bottom: rgba(60, 153, 130, 0.6) solid 5px;
+}
+
+.tab {
+    display: inline-block;
+    padding: 10px 20px;
+    cursor: pointer;
+    margin-right: 5px;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+}
+
+.active {
+    background-color: rgba(60, 153, 130, 0.6);
+    /* 选中的背景颜色 */
+    color: white;
+    /* 选中的字体颜色 */
+}
+
+.tab:hover:not(.active) {
+    background-color: rgba(84, 161, 142, 0.4);
+    /* 悬停时的背景颜色 */
+    color: white;
+    /* 悬停时的字体颜色 */
+}
 </style>
